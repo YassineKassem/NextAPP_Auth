d0f42afc84af66024a2172137cb9b8543aad85eb
YassineKassem <yassinekassem33@gmail.com>
2024-10-11 08:41:22 +0100

    fixes deploy

--- pages/profile.js
+++ pages/profile.js
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
 import { useSession, signOut } from 'next-auth/react';
 import { useState, useEffect } from 'react';
 import { supabase } from '../../utils/supabaseClient';
